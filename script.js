37aa17968893f10ee3dd4429a26b196388dff40d
dhaboav <dhaboav@gmail.com>
2023-11-14 18:32:17 +0700

    Header completed

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const menu = document.querySelector('.menu');
-    const navbarLinks = document.querySelector('.navbar-links');
-    const menuBar = document.querySelectorAll('.bar');
-
-    menu.addEventListener('click', () => {
-        menu.classList.toggle('active')
-        navbarLinks.classList.toggle('active');
-        // Toggle 'active' class on each bar element
-        menuBar.forEach(bar => {
-            bar.classList.toggle('active');
-        });
+// Menu
+document.addEventListener("DOMContentLoaded", function () {
+    const menuButton = document.querySelector(".menu");
+    const menuIcon = document.querySelector(".menu i");
+    
+    menuButton.addEventListener("click", function () {
+        if (menuIcon.classList.contains("bx-menu")) {
+            menuIcon.classList.remove("bx-menu");
+            menuIcon.classList.add("bx-x");
+        } else {
+            menuIcon.classList.remove("bx-x");
+            menuIcon.classList.add("bx-menu");
+        }
     });
 });
