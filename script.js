97892fd6d0c9d10ae1f4bef9870dbbb3feca36d9
dhaboav <dhaboav@gmail.com>
2023-11-14 19:48:33 +0700

    mobile menu sidebar

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
-// Menu
+// Menu Mobile
 document.addEventListener("DOMContentLoaded", function () {
     const menuButton = document.querySelector(".menu");
+    const navLinks = document.querySelector(".links");
     const menuIcon = document.querySelector(".menu i");
-    
+
     menuButton.addEventListener("click", function () {
+        navLinks.classList.toggle("show");
+
         if (menuIcon.classList.contains("bx-menu")) {
             menuIcon.classList.remove("bx-menu");
             menuIcon.classList.add("bx-x");
